59e61cf804f6de6cb5a76121620ef96144f6340d
Jon Melnick <jonmelnick@hotmail.com>
2016-06-28 17:54:03 -0700

    backend auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  helper_method :current_user
 
   def login(user)
     session[:session_token] = user.reset_session_token
@@ -11,14 +11,11 @@ class ApplicationController < ActionController::Base
   def logout
     current_user.reset_session_token
     session[:session_token] = nil
+    @currentUser = nil
   end
 
   def current_user
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
-  def logged_in?
-    @current_user
-  end
-  
 end
