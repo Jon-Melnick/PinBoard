c4e4e012696a20b7edf21984719dd320f3496abb
Jon Melnick <jonmelnick@hotmail.com>
2016-06-28 17:42:52 -0700

    backend auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
   def login(user)
     session[:session_token] = user.reset_session_token
@@ -17,4 +17,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
+  def logged_in?
+    @current_user
+  end
+  
 end
--- frontend/PinBoard.jsx
+++ frontend/PinBoard.jsx
@@ -1,16 +1,52 @@
 const React = require('react'),
-      ReactDOM = require('react-dom');
+      ReactDOM = require('react-dom'),
+      ReactRouter = require('react-router'),
+      Router = ReactRouter.Router,
+      Route = ReactRouter.Route,
+      IndexRoute = ReactRouter.IndexRoute,
+      hashHistory = require('react-router').hashHistory;
+
+const Login = require('./components/login_form'),
+      Signup = require('./components/signup_form'),
+      SessionsStore = require('./stores/session_store'),
+      SessionsActions = require('./actions/session_actions'),
+      Empty = require('./components/empty');
 window.SessionApi = require('./util/session_api_util');
+window.SessionsStore = require('./stores/session_store');
 
 
 const App = React.createClass({
   render(){
     return (
-      <div></div>
+      <div className="app">
+        {this.props.children}
+      </div>
     )
   }
 });
 
+const routes = (
+  <Router history={ hashHistory }>
+    <Route path="/" component={ App }>
+      <IndexRoute component={ Empty } />
+      <Route path="/login" component={ Login } />
+      <Route path="/empty" component={ Empty } />
+      <Route path="/signup" component={ Signup } />
+    </Route>
+  </Router>
+)
+
+
+function _ensureLoggedIn(nextState, replace) {
+    if (!SessionStore.isUserLoggedIn()) {
+      replace('/login');
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<div>Yo! lets get things started, Mr. Melnick!</div>, document.getElementById('content'))
+  if (window.currentUser) {
+    SessionsActions.receiveCurrentUser(window.currentUser);
+  }
+
+  ReactDOM.render(routes, document.getElementById('content'))
 });
